6940a1c1675df83d6396fc8ee8e7af01cc75eb38
akhmadarief <w.akhmadarief@gmail.com>
2019-11-18 19:00:44 +0700

    Update get_city.php

--- get_city.php
+++ get_city.php
@@ -1,11 +1,14 @@
 <?php
-	include 'conf.php';
-	$id_provinsi = $_POST['id_provinsi'];
+	include "conf.php";
 
 	echo "<option disabled='disabled' selected='selected'>Pilih Kota/Kabupaten</option>";
 
-	$kota = mysqli_query($conn,"SELECT * FROM regencies WHERE province_id='$id_provinsi' ORDER BY name ASC");
-	while ($row = mysqli_fetch_assoc($kota)) {
-		echo "<option value='" . $row['id'] . "'>" . $row['name'] . "</option>";
+	$kota = $conn->prepare("SELECT * FROM regencies WHERE province_id=? ORDER BY name ASC");
+	$kota->bind_param("i", $_POST['id_provinsi']);
+	$kota->execute();
+	$result = $kota->get_result();
+	while($row = $result->fetch_assoc()) {
+		echo "<option value='".$row['id']."'>".$row['name']."</option>";
 	}
+	$kota->close();
 ?>
