173fe620dea2e3a547e5e63ed554133801387739
akhmadarief <w.akhmadarief@gmail.com>
2019-11-18 19:00:51 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,24 +1,13 @@
 <?php
+	include "conf.php";
 
-include("conf.php");
+	$stmt = $conn->prepare("INSERT INTO peserta SET nama=?, id=?, inst=?, jenis_kelamin=?, email=?, hp=?, id_kota=?, id_seminar=?");
+	$stmt->bind_param("ssssssii", $_POST['nama'], $_POST['no_id'], $_POST['inst'], $_POST['gender'], $_POST['email'], $_POST['hp'], $_POST['kota'], $_POST['seminar']);
+	$stmt->execute();
 
-$nama = mysqli_real_escape_string($conn, $_POST['nama']);
-$id = mysqli_real_escape_string($conn, $_POST['no_id']);
-$inst = mysqli_real_escape_string($conn, $_POST['inst']);
-$jk = mysqli_real_escape_string($conn, $_POST['gender']);
-$email = mysqli_real_escape_string($conn, $_POST['email']);
-$hp = mysqli_real_escape_string($conn, $_POST['hp']);
-$id_kota = mysqli_real_escape_string($conn, $_POST['kota']);
-$id_seminar = mysqli_real_escape_string($conn, $_POST['seminar']);
+	if ($stmt){
+		header('location: index.php');
+	}
 
-$query = mysqli_query($conn, "INSERT INTO peserta SET nama='$nama', id=$id, inst='$inst', jenis_kelamin='$jk', email='$email', hp='$hp', id_kota=$id_kota, id_seminar=$id_seminar")
-or die(mysqli_error($conn));
-
-if ($query){
-	header('location: index.php');
-}
-else{
-	echo mysqli_error($conn);
-}
-
+	$stmt->close();
 ?>
